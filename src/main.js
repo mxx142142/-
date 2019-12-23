6a885465a504cb2f3698a4443e523b2ad99d3fcc
unknown <mxx@example.com>
2019-12-23 11:52:57 +0800

    目录

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 日志的详细与否 生产日志
 
+Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+// main.js入口文件
+// 职责.做全局范围内的依赖和配置
+// 创建一个根vue实例
